bfb5870c10a53a7ee8e3021faa242f56d10e6ff2
balexa16 <balexa16@hills.ccsf.edu>
2019-01-19 12:47:51 -0800

    This is a test file

---
+++ add.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	float user1, user2;
+	//these store user input values
+	cout << "enter a number: " << endl;
+	cin >> user1;
+	cout << "enter a number: " << endl;
+	cin >> user2;
+	cout << "the sum is: " << user1 + user2 << endl;
+	
+}
