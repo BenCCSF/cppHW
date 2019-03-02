959370580e6f6680974b903fe2e96e911b5d29c8
balexa16 <balexa16@hills.ccsf.edu>
2019-03-02 09:23:21 -0800

    Added user array ask program

---
+++ midTerm/userArr.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void getNum(int &target) {
+    //Takes a number as input from the user: stores in int
+    cin >> target;
+    while (!cin) {
+        //Cin error handleing
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Please enter a valid number" << endl;
+        cin >> target;
+    }
+    cin.ignore(1000, '\n');
+}
+void getNum(float &target) {
+    //Takes a number as input from the user: stores in float
+    cin >> target;
+    while (!cin) {
+        //Cin error handleing
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Please enter a valid number" << endl;
+        cin >> target;
+    }
+    cin.ignore(1000, '\n');
+}
+
+int main() {
+    int arrLen;
+    cout << "How many numbers would you like to enter? " << endl;
+    getNum(arrLen);
+    float arr[arrLen];
+    for (int i=0; i<arrLen; i++) {
+        cout << "Enter a number: ";
+        getNum(arr[i]);
+    }
+    //Now print the array
+    cout << "You entered: " << endl;
+    for (int i=0; i<arrLen; i++) {
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
+}
