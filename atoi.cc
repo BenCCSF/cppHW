7e0e100c35853bbd33a9f13e0d9e099e69882673
balexa16 <balexa16@hills.ccsf.edu>
2019-05-11 11:15:06 -0700

    Improved readablity

--- atoi.cc
+++ atoi.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <cstdlib>
 using namespace std;
 
 int _atoi(const char * str) {
@@ -7,6 +6,7 @@ int _atoi(const char * str) {
     int total = 0; 
     while (*str && *str <= 57 && *str >= 48) {
         total = total * 10;
+	//ASCII codes for integers are from 48(0) to 57 (9)
         total += (*str - 48);
         str++;
     }
@@ -17,8 +17,8 @@ int _atoi(const char * str) {
 
 int main() {
     char c[32] = "123";
+    //Just an example string to convert
     int d = _atoi(c);
     cout << d << endl;
-
     return 0;
 }
