4de007db2e770075bc474c7dadde42de05b415cb
balexa16 <balexa16@hills.ccsf.edu>
2019-05-18 10:37:39 -0700

    added test program

--- finalExam/muniInheritance/main.cc
+++ finalExam/muniInheritance/main.cc
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-    //Main doesn't do anything yet: see other files
+    //First we create a cable car object
+    CableCar testCar;
+    // Now pay two fares
+    for (int i=0; i<2; i++) {
+        testCar.paidFare();
+    }
+    //Finally: print out the total fares
+    cout << "The total fare is: $" << testCar.getTotalFares() << endl;
+
     return 0;
 }
